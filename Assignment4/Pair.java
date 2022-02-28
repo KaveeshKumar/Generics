c681d5d8cd5e54c566442f63360e3cd38b018cc7
KaveeshKumar <28Kaveesh@gmail.com>
2022-02-28 22:02:40 +0530

    Generics assignment is commited

---
+++ Assignment4/Pair.java
@@ -0,0 +1,37 @@
+class Pair<T>{
+
+    private T key;
+
+    public T getKey() {
+        return key;
+    }
+
+    public void setKey(T key) {
+        this.key = key;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    private T value;
+
+    @Override
+    public String toString() {
+        return "Pair = {" +
+                "key=" + key +
+                ", value=" + value +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Pair<String> pair = new Pair<>();
+        pair.setKey("1");
+        pair.setValue("Hello");
+        System.out.println(pair);
+    }
+}
